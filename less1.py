c3eea4cb7ccee5b85ecfcda0c022088921099d1a
Yakov Boiev <user.name>
2021-11-15 15:26:00 +0200

    create test_ping

--- less1.py
+++ less1.py
@@ -1,3 +1,7 @@
+import subprocess
+import chardet
+
+
 def type_print(*args):
     for word in args:
         print(type(word), word)
@@ -22,22 +26,33 @@ def not_write_byte_type(*args):
 def str_byte_transform(*args):
     output_list = []
     for word in args:
-        output_list.append(word.encode('utf-8'))
+        output_list.append(word.encode(encoding='utf-8'))
     return output_list
 
 
 def byte_str_transform(byte_list):
     output_list = []
     for word in byte_list:
-        output_list.append((word.decode('utf-8')))
+        output_list.append((word.decode(encoding='utf-8')))
     return output_list
 
 
-type_print('разработка', 'сокет', 'декоратор')
-type_print('\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430', '\u0441\u043e\u043a\u0435\u0442',
-           '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440')
-transform_bite_format('class', 'function', 'method')
-print(not_write_byte_type('attribute', 'класс', 'функция', 'type'))
-byte_list = str_byte_transform('разработка', 'администрирование', 'protocol', 'standard')
-print(byte_list)
-print(byte_str_transform(byte_list))
+def test_ping(*args):
+    for site in args:
+        subproc_arg = ['ping', '-4', site]
+        subprocess_ping = subprocess.Popen(subproc_arg, stdout=subprocess.PIPE)
+        for line in subprocess_ping.stdout:
+            result = chardet.detect(line)
+            line = line.decode(result['encoding']).encode('utf-8')
+            print(line.decode('utf-8'))
+
+
+# type_print('разработка', 'сокет', 'декоратор')
+# type_print('\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430', '\u0441\u043e\u043a\u0435\u0442',
+#            '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440')
+# transform_bite_format('class', 'function', 'method')
+# print(not_write_byte_type('attribute', 'класс', 'функция', 'type'))
+# byte_list = str_byte_transform('разработка', 'администрирование', 'protocol', 'standard')
+# print(byte_list)
+# print(byte_str_transform(byte_list))
+test_ping('www.yandex.ru', 'www.youtube.com')
