200cee336b1def2ce5d7902c0673a76e5f7a7665
yakov <iakovboiev@gmail.com>
2021-12-02 20:03:45 +0200

    finish coding less5

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 """Программа-клиент"""
-
+import argparse
+import logging
 import sys
 import json
 import socket
@@ -7,6 +8,10 @@ import time
 from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
     RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
 from common.utils import get_message, send_message
+from errors import ReqFieldMissingError
+import logs.config_client_log
+
+CLIENT_LOGGER = logging.getLogger('client')
 
 
 def create_presence(account_name='Guest'):
@@ -22,6 +27,7 @@ def create_presence(account_name='Guest'):
             ACCOUNT_NAME: account_name
         }
     }
+    CLIENT_LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
     return out
 
 
@@ -31,38 +37,52 @@ def process_ans(message):
     :param message:
     :return:
     '''
+    CLIENT_LOGGER.debug(f'Разбор сообщения от сервера: {message}')
     if RESPONSE in message:
         if message[RESPONSE] == 200:
             return '200 : OK'
         return f'400 : {message[ERROR]}'
-    raise ValueError
+    raise ReqFieldMissingError(RESPONSE)
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
+    parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+    return parser
 
 
 def main():
     '''Загружаем параметы коммандной строки'''
-    try:
-        server_address = sys.argv[1]
-        server_port = int(sys.argv[2])
-        if server_port < 1024 or server_port > 65535:
-            raise ValueError
-    except IndexError:
-        server_address = DEFAULT_IP_ADDRESS
-        server_port = DEFAULT_PORT
-    except ValueError:
-        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+
+    parser = create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+    if server_port < 1024 or server_port > 65535:
+        CLIENT_LOGGER.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}.'
+            f' Допустимы адреса с 1024 до 65535. Клиент завершается.')
         sys.exit(1)
 
-    # Инициализация сокета и обмен
+    CLIENT_LOGGER.info(f'Запущен клиент с парамертами: '
+                       f'адрес сервера: {server_address} , порт: {server_port}')
 
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
-    message_to_server = create_presence()
-    send_message(transport, message_to_server)
+    # Инициализация сокета и обмен
     try:
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        message_to_server = create_presence()
+        send_message(transport, message_to_server)
         answer = process_ans(get_message(transport))
-        print(answer)
-    except (ValueError, json.JSONDecodeError):
-        print('Не удалось декодировать сообщение сервера.')
+        CLIENT_LOGGER.info(f'Принят ответ от сервера {answer}')
+    except json.JSONDecodeError:
+        CLIENT_LOGGER.error('Не удалось декодировать полученную Json строку.')
+    except ConnectionRefusedError:
+        CLIENT_LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+                               f'конечный компьютер отверг запрос на подключение.')
+    except ReqFieldMissingError as missing_error:
+        CLIENT_LOGGER.error(f'В ответе сервера отсутствует необходимое поле '
+                            f'{missing_error.missing_field}')
 
 
 if __name__ == '__main__':
--- logs/config_client_log.py
+++ logs/config_client_log.py
@@ -1,8 +1,9 @@
 import sys
 import os
 import logging
-sys.path.append('../')
 from common.variables import LOGGING_LEVEL
+sys.path.append('../')
+
 
 CLIENT_FORMATTER = logging.Formatter('%(asctime)s %(levelname)-8s %(filename)s %(message)s')
 PATH = os.path.dirname(os.path.abspath(__file__))
@@ -11,6 +12,8 @@ PATH = os.path.join(PATH, 'client.log')
 STREAM_HANDLER = logging.StreamHandler(sys.stderr)
 STREAM_HANDLER.setFormatter(CLIENT_FORMATTER)
 STREAM_HANDLER.setLevel(logging.ERROR)
+LOG_FILE = logging.FileHandler(PATH, encoding='utf8')
+LOG_FILE.setFormatter(CLIENT_FORMATTER)
 
 LOGGER = logging.getLogger('client')
 LOGGER.addHandler(STREAM_HANDLER)
